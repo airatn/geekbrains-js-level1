fdbd42818ae7b6be8337316061c4670474d8099e
Айрат Нурутдинов <airatn@gmail.com>
2016-11-12 15:00:43 +0300

    7 урок

--- main.js
+++ main.js
@@ -228,3 +228,100 @@ function ainimationResult (result, idDiv) {
 
         return HTMLstring;
     }
+
+
+// 7-ой урок
+
+    //
+    function lChess(i) {
+        var liters = ["A","B","C","D","E","F","G","H"];
+        return liters[i-1];
+    }
+
+    // html разметка идентичная первому заданию (вынесена в функцию)
+    function chessBoard() {
+
+        var html = "";
+        //Добавляем верхную буквенную нумерация столбца
+        html += "<div  style='float: left; width: 18px; height: 12px;'></div>";
+        for (var j=1; j<=8; j++) {
+            html += "<div style='float: left; width: 50.5px; padding 0px 0; text-align: center; border-bottom: solid 2px black;'>"+ lChess(j) + "</div>";
+        }
+        <!-- Отмена обтекания -->
+        html += "</div><div style='clear: both;'></div>";
+        <!-- Основное поле -->
+        for (var i=1; i<=8; i++) {
+            html += "<div>";
+            <!-- Цифра поля по вертикали -->
+            html += "<div  style='float: left; padding: 0 5px; line-height: 50px; border-right: solid 2px black;'>" + (9-i) + "</div>";
+            for (var j=1; j<=4; j++) {
+                if (i%2 == 0) {
+                    //Первый вид сочетания двух клеток (бело-черное поля)
+                    html += "<div style='float: left; height: 50px; width: 50px; background-color: RGB(225, 201, 170);'></div>";
+                    html += "<div style='float: left; height: 50px; width: 50px; background-color: RGB(156, 86, 35);'></div>";
+                } else {
+                    //Второй вид сочетания двух клеток (черное-белое поля)
+                    html += "<div style='float: left; height: 50px; width: 50px; background-color: RGB(156, 86, 35);'></div>";
+                    html += "<div style='float: left; height: 50px; width: 50px; background-color: RGB(225, 201, 170);'></div>";
+                }
+            }
+            <!-- Цифра поля по вертикали -->
+            html += "<div  style='float: left; padding: 0 5px; line-height: 50px; border-left: solid 2px black;'>" + (9-i) + "</div>";
+            <!-- Отмена обтекания -->
+            html += "</div><div style='clear: both;'></div>";
+        }
+        //Добавляем нижнию буквенную нумерация столбца
+        html += "<div  style='float: left; width: 18px; height: 12px;'></div>";
+        for (var j=1; j<=8; j++) {
+            html += "<div style='float: left; width: 50.5px; padding 0px 0; text-align: center; border-top: solid 2px black;'>"+ lChess(j) + "</div>";
+        }
+
+        return html;
+    }
+
+    //Возвращает html разметку в формате "<img src='images/chess/castleB.svg' style='width:30px;'>"
+    //в зависимости от передавамой фигуры в формате "Rb", "Kb", "Bb", "Qb", "Kb", "Bb", "Kb", "Rb"
+    function chessPieceHTML(piece) {
+        switch (piece) {
+            case 'Rb':
+                return "<img src='images/chess/castleB.svg' style='width:30px;'>";
+                break;
+            case 'Knb':
+                return "<img src='images/chess/knightB.svg' style='width:34px;'>";
+                break;
+            case 'Bb':
+                return "<img src='images/chess/bishopB.svg' style='width:34px;'>";
+                break;
+            case 'Qb':
+                return "<img src='images/chess/queenB.svg' style='width:36px;'>";
+                break;
+            case 'Kb':
+                return "<img src='images/chess/kingB.svg' style='width:34px;'>";
+                break;
+            case 'Pb':
+                return "<img src='images/chess/pawnB.svg' style='width:30px;'>";
+                break;
+
+            case 'Rw':
+                return "<img src='images/chess/castleW.svg' style='width:30px;'>";
+                break;
+            case 'Knw':
+                return "<img src='images/chess/knightW.svg' style='width:34px;'>";
+                break;
+            case 'Bw':
+                return "<img src='images/chess/bishopW.svg' style='width:34px;'>";
+                break;
+            case 'Qw':
+                return "<img src='images/chess/queenW.svg' style='width:36px;'>";
+                break;
+            case 'Kw':
+                return "<img src='images/chess/kingW.svg' style='width:34px;'>";
+                break;
+            case 'Pw':
+                return "<img src='images/chess/pawnW.svg' style='width:30px;'>";
+                break;
+            default:
+                return "";
+                break;
+        }
+    }
